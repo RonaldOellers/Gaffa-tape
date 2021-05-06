cc56ed44f368cd925e459b1496833a6760c4cd7d
RonaldOellers <Ronald.j.w.Oellers@gmail.com>
2021-05-06 17:41:34 +0200

    updated the renamefiles.py

--- code/python/renamefiles.py
+++ code/python/renamefiles.py
@@ -1,10 +1,10 @@
+#script for renameming SRR files downloaded from the GEO database (search at: ) into a format workable by downstream applications. This makes use of a (,) comma seperated .csv file as a dictionary (format: Experiment Title,SSR accession Number\n xxxxxx,SRRxxxxxx)
 import os
 infile = input("Which file is the renaming dictionary?")
-#infile = 'renaming2.csv'
 file = open(infile, 'r')
 lines = file.readlines()[1:]
 for line in lines:
-        line = line.strip() #IMPORTANT TO REMOVE THE SPACES ETC. ELSE TESTING DOES NOT WORK
+        line = line.strip() #IMPORTANT TO REMOVE THE SPACES ETC. ELSE IT DOES NOT WORK
         line = line.split(',')
         #print(line[1])
         for fname in os.listdir('./'):
