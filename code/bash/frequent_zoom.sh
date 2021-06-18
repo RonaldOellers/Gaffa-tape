716f3e7869d0747a01cde6cf9eabb93c701788c2
RonaldOellers <Ronald.j.w.Oellers@gmail.com>
2021-06-18 17:55:24 +0200

    updates code

--- code/bash/frequent_zoom.sh
+++ code/bash/frequent_zoom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Note you will have to replace the corresponding section with a zoomlink that does not change and that you frequently attend eg. https://zoom.us/my/XXXXXX
-xdg-open REPLACE_THIS_WITH_NONCHANGING_ZOOMLINK
+xdg-open #REPLACE_THIS_WITH_NONCHANGING_ZOOMLINK
