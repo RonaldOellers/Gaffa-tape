3f519fc348cca998d19803767cd168e3a0a83616
RonaldOellers <Ronald.j.w.Oellers@gmail.com>
2022-03-31 12:29:58 +0200

    added node status to refresh_sinfo

--- code/bash/refresh _sinfo.sh
+++ code/bash/refresh _sinfo.sh
@@ -2,7 +2,7 @@
 module load slurm
 while 1>0; do
 clear
-sinfo -o "%.9n %.9P %.13C %.8O %.7m %.7e"
+sinfo -o "%.9n %.9P %.7t %.13C %.8O %.7m %.7e"
 #sleep 30s #refresh every 30 seconds
 read -t 30 key #refresh every 30 seconds or after pressing enter
 done
